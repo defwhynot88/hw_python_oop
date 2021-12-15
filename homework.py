3ecf8ff671e2279c6ff2ae1149dcc42a34522d40
Виктория <def.whynot@yandex.ru>
2021-12-15 13:30:57 +0300

    Пока работает - не трогай

--- homework.py
+++ homework.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, fields
 from typing import ClassVar
 
 
@@ -10,16 +10,16 @@ class InfoMessage:
     distance: float
     speed: float
     calories: float
-    PYTKA_POPYTKA: ClassVar[str] = (   # я не понимаю как тут надо исправить..можете накинуть материал или перефразировать комментарий?
-        f'Тип тренировки: {training_type}; '
-        f'Длительность: {duration:.{3}f} ч.; '
-        f'Дистанция: {distance:.{3}f} км; '
-        f'Ср. скорость: {speed:.{3}f} км/ч; '
-        f'Потрачено ккал: {calories:.{3}f}'
+    PYTKA_POPYTKA: ClassVar[str] = (
+        'Тип тренировки: {}; '
+        'Длительность: {:.3f} ч.; '
+        'Дистанция: {:.3f} км; '
+        'Ср. скорость: {:.3f} км/ч; '
+        'Потрачено ккал: {:.3f}.'
     )
 
-    def get_message(self) -> str:  # ??
-        return self.PYTKA_POPYTKA(
+    def get_message(self) -> str:
+        return self.PYTKA_POPYTKA.format(
             self.training_type, self.duration,
             self.distance, self.speed,
             self.calories
@@ -29,9 +29,9 @@ class InfoMessage:
 @dataclass
 class Training:
     """Базовый класс тренировки."""
-    LEN_STEP = 0.65
-    M_IN_KM = 1000
-    H_IN_M = 60
+    LEN_STEP: ClassVar[float] = 0.65
+    M_IN_KM: ClassVar[int] = 1000
+    H_IN_M: ClassVar[int] = 60
 
     action: int
     duration: float
@@ -62,82 +62,83 @@ class Training:
 
 class Running(Training):
     """Тренировка: бег."""
-    MUR_MUR_1 = 18
-    MUR_MUR_2 = 20
+    MUR_MUR_1: ClassVar[float] = 18
+    MUR_MUR_2: ClassVar[float] = 20
 
     def get_spent_calories(self) -> float:
-        """Получить количество затраченных калорий."""
-        return ((self.MUR_MUR_1 * self.get_mean_speed() - self.MUR_MUR_2)
-                * self.weight / self.M_IN_KM * self.duration * self.H_IN_M)
+        return (((self.MUR_MUR_1 * self.get_mean_speed())
+                - self.MUR_MUR_2) * self.weight / self.M_IN_KM
+                * self.duration * self.H_IN_M)
 
 
 @dataclass
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
+    PUP_PUP_1: ClassVar[float] = 0.035
+    PUP_PUP_2: ClassVar[float] = 0.029
     height: float
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        PUP_PUP_1 = 0.035
-        PUP_PUP_2 = 0.029
-
-        return (((PUP_PUP_1 * self.weight)
-                + ((self.get_mean_speed() ** 2 // self.height)
-                * PUP_PUP_2 * self.weight))
+        return (((self.PUP_PUP_1 * self.weight)
+                + ((self.get_mean_speed()**2 // self.height)
+                * self.PUP_PUP_2 * self.weight))
                 * self.duration * self.H_IN_M)
 
 
 @dataclass
 class Swimming(Training):
     """Тренировка: плавание."""
-    LEN_STEP = 1.38
+    WOB_WOB_1: ClassVar[float] = 1.1
+    WOB_WOB_2: ClassVar[float] = 2
+    LEN_STEP: ClassVar[float] = 1.38
     length_pool: float
     count_pool: int
 
     def get_mean_speed(self) -> float:
-        """Получить среднюю скорость движения."""
+        """Средняя скорость."""
         return (
             self.length_pool * self.count_pool
             / self.M_IN_KM / self.duration
         )
 
     def get_spent_calories(self) -> float:
-        """Получить количество затраченных калорий."""
-        WOB_WOB_1 = 1.1
-        WOB_WOB_2 = 2
-
+        """Спаленные калории"""
         return (
-            (self.get_mean_speed() + WOB_WOB_1)
-            * WOB_WOB_2 * self.weight
+            (self.get_mean_speed() + self.WOB_WOB_1)
+            * self.WOB_WOB_2 * self.weight
         )
+
+
+TRAINING_TYPE = {'SWM': Swimming, 'RUN': Running, 'WLK': SportsWalking}
+TRAINING_ERROR_404 = 'Упс, нет такой тренировки {}'
 
 
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
-    TYPE = {
-        'WLK': SportsWalking,
-        'RUN': Running,
-        'SWM': Swimming
-    }
-    ERROR_404 = 'Упс, нет такой тренировки {}'
-
-    if workout_type in TYPE:
-        active_class = TYPE.get(workout_type)
-        info_error(active_class, data)
-        return TYPE.get(workout_type)(*data)
-    raise ValueError(ERROR_404.format(workout_type))
+    if workout_type in TRAINING_TYPE:
+        tested_class = TRAINING_TYPE.get(workout_type)
+        error_info(tested_class, data)
+        return TRAINING_TYPE.get(workout_type)(*data)
+    raise ValueError(TRAINING_ERROR_404.format(workout_type))
 
 
 def main(training: Training) -> None:
     """Главная функция."""
-    print(Training.show_training_info.get_message())
+    print(training.show_training_info().get_message())
+
+
+def error_info(tested_class, data):
+    number_of_class_fields = len(fields(tested_class))
+    if number_of_class_fields != len(data):
+        raise TypeError('Ожиалось другое число аргументов')
 
 
 if __name__ == '__main__':
     packages = [
         ('SWM', [720, 1, 80, 25, 40]),
         ('RUN', [15000, 1, 75]),
-        ('WLK', [9000, 1, 75, 180]),
+        ('WLK', [9000, 1, 75, 180])
     ]
 
     for workout_type, data in packages:
